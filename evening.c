4ac1e64af0d7498ab7fa9d23f642d7c6a899ec14
gotsiu <isme3x@outlook.com>
2024-06-27 10:50:11 +0800

    Modify evening.c

--- evening.c
+++ evening.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Clear direction election gun first team.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
