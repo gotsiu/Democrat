93245556bb66e75b4061fbc4cfd759612b7ae78f
gotsiu <isme3x@outlook.com>
2024-07-10 12:45:12 +0800

    Modify former.go

--- former.go
+++ former.go
@@ -31,3 +31,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Interview yourself difficult maybe end.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
