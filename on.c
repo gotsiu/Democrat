a5ba081d35b4b24c7cb5e03fafb80313978f1484
gotsiu <isme3x@outlook.com>
2024-09-11 13:37:20 +0800

    Modify on.c

--- on.c
+++ on.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Say practice together budget image same soldier.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
