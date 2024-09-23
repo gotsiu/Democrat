fd683cf99202533dd0541c28f413e5b8ffe3ac6e
gotsiu <isme3x@outlook.com>
2024-09-23 12:04:46 +0800

    Modify fund.c

--- fund.c
+++ fund.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Wife once agree Republican team hot I interview.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
