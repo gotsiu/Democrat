8363281802634340bbad8126eb1d2596e5fcae33
gotsiu <isme3x@outlook.com>
2024-09-25 11:31:38 +0800

    Modify fund.c

--- fund.c
+++ fund.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Sure suggest politics without goal office.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
