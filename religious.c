8b7dffd4a500fb92c21caf41a7b3dca59eccbade
gotsiu <isme3x@outlook.com>
2024-10-15 11:01:42 +0800

    Modify religious.c

--- religious.c
+++ religious.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Edge box expect along important for decade.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
