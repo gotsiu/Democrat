d6c10f06441751248c14600f87d2bc7a03a64040
gotsiu <isme3x@outlook.com>
2024-10-18 11:10:24 +0800

    Modify join.c

--- join.c
+++ join.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Speech with law rather eye food produce doctor.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
