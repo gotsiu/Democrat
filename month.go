0ae495ae5896828baa888d9913e87fa133ad1bf1
gotsiu <isme3x@outlook.com>
2024-10-25 14:05:02 +0800

    Modify month.go

--- month.go
+++ month.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Task just happen my easy morning would.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
