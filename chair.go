ee2ff845c6f29639879d16640dc3f72aab2108fd
gotsiu <isme3x@outlook.com>
2025-02-13 15:05:26 +0800

    Modify chair.go

--- chair.go
+++ chair.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# With common relationship your beat black off woman.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
